67c01b50ac200833fc492cff07b016218930ac0f
Edinaldo <edinaldolemos@yahoo.com.br>
2021-08-21 17:22:34 -0300

    calculo-com-useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
 function App() {
+
+  const [numero,setNumero ] = useState(0);
+  const [segundoNumero, setSegungoNumero] = useState(0);
+  const [resultado, setResultado] = useState(0);
+
+   const somar = () => {
+      let numeroInt = parseInt(numero);
+      let numero2Int = parseInt(segundoNumero);
+
+      setResultado(numeroInt + numero2Int);
+   }
+  /*Podemos usar desta forma também */
+
+  // const [state, setState] =useState({
+  // numero:0,
+  // segundoNumero: 0,
+  // resultado:0
+
+  // });
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+    Numero 1: <input type="text" value={numero} 
+    onChange={e=> setNumero(e.target.value)}/><br/>
+    Numero 2: <input type="text" value={segundoNumero} 
+    onChange={e=> setSegungoNumero(e.target.value)}/><br/>
+    <button type="button" onClick={somar}>Calcular</button><br/>
+    Resultado: <input type="text" value={resultado}/>
+  
+    </>
+  
   );
 }
 
